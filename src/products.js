892d68633bef4614e7928507af462049535afa15
SeanyC <sean.collier928@yahoo.com>
2024-01-09 12:08:45 -0800

    dummy images added

--- src/products.js
+++ src/products.js
@@ -1,9 +1,9 @@
 // products.js
 const products = [
-    { id: 1, name: 'Product 1', price: 50, imageUrl: '' },
-    { id: 2, name: 'Product 2', price: 150, imageUrl: '' },
-    { id: 3, name: 'Product 3', price: 200, imageUrl: '' },
-    { id: 4, name: 'Product 4', price: 300, imageUrl: '' },
+    { id: 1, name: 'Product 1', price: 50, imageUrl: './assets/dummyimage1.png' },
+    { id: 2, name: 'Product 2', price: 150, imageUrl: './assets/dummyimage2.png' },
+    { id: 3, name: 'Product 3', price: 200, imageUrl: './assets/dummyimage3.png' },
+    { id: 4, name: 'Product 4', price: 300, imageUrl: './assets/dummyimage4.png' },
     // Add more products as needed
   ];
 
